de473940d2a3386e0ec130154fd10e1423ff0e4e
yttap8 <moc.liamg.patrick@gmail.com>
2024-12-06 01:08:42 -0500

    header

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export function Header({ isVisible }: { isVisible: boolean }) {
   if (!isVisible) return null;
   return (
-    <header className="fixed top-0 left-0 right-0 bg-white border-b z-10">
+    <header className="fixed top-0 left-0 right-0 bg-white border-b z-20">
       <div className="p-4 flex items-center justify-between">
         <div className="flex items-center space-x-2">
           <img

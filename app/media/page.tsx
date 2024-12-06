eb78be90069cb5c23c3a756a5fe38bf760c57e6b
Phillip-Gao <phillipgao10@gmail.com>
2024-12-06 01:02:11 -0500

    edit media page

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import React from "react";
 
 export default function MediaPage() {
   return (
@@ -6,9 +6,10 @@ export default function MediaPage() {
       <h2 className="text-2xl font-bold mb-4">TRENDING POSTS</h2>
 
       <div className="flex flex-col gap-8">
+        {/* House of Highlights Section */}
         <div className="max-w-full">
           <div className="flex items-center gap-3 mb-4">
-            <Image
+            <img
               src="/Media/househighlights.png"
               alt={"House of Highlights"}
               width={40}
@@ -18,34 +19,35 @@ export default function MediaPage() {
             <h3 className="text-xl text-gray-500">House of Highlights</h3>
           </div>
 
-          <div className="flex justify-between gap-2">
-            <Image
-              src="/Media/post1.png"
-              alt={`House of Highlights`}
-              width={30}
-              height={30}
-              className="w-full"
-            />
-            <Image
-              src="/Media/post2.png"
-              alt={`House of Highlights`}
-              width={30}
-              height={30}
-              className="w-full"
-            />
-            <Image
-              src="/Media/post3.png"
-              alt={`House of Highlights`}
-              width={30}
-              height={30}
-              className="w-full"
-            />
+          {/* Two Posts Per Row with Padding */}
+          <div className="grid grid-cols-2 gap-8">
+            <iframe
+              src="https://www.instagram.com/p/DC0N3l8Nvu0/embed"
+              width="150"
+              height="150"
+              allowTransparency="true"
+              allow="encrypted-media"
+              scrolling="no"
+              frameBorder="0"
+              style={{ borderRadius: '8px' }}
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/DC0N3l8Nvu0/embed"
+              width="150"
+              height="150"
+              allowTransparency="true"
+              allow="encrypted-media"
+              scrolling="no"
+              frameBorder="0"
+              style={{ borderRadius: '8px' }}
+            ></iframe>
           </div>
         </div>
 
+        {/* Bleacher Report Section */}
         <div className="max-w-full">
           <div className="flex items-center gap-3 mb-4">
-            <Image
+            <img
               src="/Media/br.png"
               alt={"Bleacher Report"}
               width={40}
@@ -55,34 +57,36 @@ export default function MediaPage() {
             <h3 className="text-xl text-gray-500">Bleacher Report</h3>
           </div>
 
-          <div className="flex justify-between gap-2">
-            <Image
-              src="/Media/post4.png"
-              alt={`Bleacher Report`}
-              width={30}
-              height={30}
-              className="w-full"
-            />
-            <Image
-              src="/Media/post5.png"
-              alt={`Bleacher Report`}
-              width={30}
-              height={30}
-              className="w-full"
-            />
-            <Image
-              src="/Media/post6.png"
-              alt={`Bleacher Report`}
-              width={30}
-              height={30}
-              className="w-full"
-            />
+          {/* Two Posts Per Row with Padding */}
+          <div className="grid grid-cols-2 gap-8">
+            <iframe
+              src="https://www.instagram.com/p/DC0N3l8Nvu0/embed"
+              width="150"
+              height="150"
+              allowTransparency="true"
+              allow="encrypted-media"
+              scrolling="no"
+              frameBorder="0"
+              style={{ borderRadius: '8px' }}
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/DC0N3l8Nvu0/embed"
+              width="150"
+              height="150"
+              allowTransparency="true"
+              allow="encrypted-media"
+              scrolling="no"
+              frameBorder="0"
+              style={{ borderRadius: '8px' }}
+            ></iframe>
           </div>
         </div>
+
+        {/* NBA Twitter Section */}
         <div>
           <h2 className="text-2xl font-bold mb-4">LATEST TWEETS</h2>
           <div className="flex items-center gap-3 mb-4">
-            <Image
+            <img
               src="/Media/nba.jpg"
               alt="NBA"
               width={40}
@@ -92,28 +96,14 @@ export default function MediaPage() {
             <h3 className="text-xl text-gray-500">NBA</h3>
           </div>
 
-          <div className="flex flex-col gap-4">
-            <Image
-              src="/Media/tweet1.png"
-              alt="NBA"
-              width={10}
-              height={10}
-              className="w-full"
-            />
-            <Image
-              src="/Media/tweet2.png"
-              alt="NBA"
-              width={10}
-              height={10}
-              className="w-full"
-            />
-            <Image
-              src="/Media/tweet3.png"
-              alt="NBA"
-              width={10}
-              height={10}
-              className="w-full"
-            />
+          {/* Two Tweets Per Row with Padding */}
+          <div className="grid grid-cols-2 gap-8">
+            <blockquote className="twitter-tweet" data-lang="en">
+              <a href="https://twitter.com/USER/status/TWEET_ID1"></a>
+            </blockquote>
+            <blockquote className="twitter-tweet" data-lang="en">
+              <a href="https://twitter.com/USER/status/TWEET_ID2"></a>
+            </blockquote>
           </div>
         </div>
       </div>
